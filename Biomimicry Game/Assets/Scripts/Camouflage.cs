ee9d401cfac43fa9d034c520bdd91cefbc7d5d40
Patnjop <36369847+Patnjop@users.noreply.github.com>
2018-02-11 14:41:12 +0000

    Moved muh stuff in
    Hope you don't mind

---
+++ Biomimicry Game/Assets/Scripts/Camouflage.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camouflage: MonoBehaviour
+{
+    Color color;
+    public KeyCode change;
+    public double abilitypower;
+    public bool isInvisible;
+    public bool inProgress;
+
+    private void Start()
+    {
+        color = GetComponent<Renderer>().material.color;
+    }
+    private void Update()
+    {
+        if (Input.GetKey(change))
+        {
+            if (isInvisible == true && inProgress == false)
+            {
+                isInvisible = false;
+                inProgress = true;
+            }
+            else if (isInvisible == false && inProgress == false && abilitypower > 0)
+            {
+                isInvisible = true;
+                inProgress = true;
+            }
+        }
+        if (isInvisible == true)
+        {
+            GetComponent<Renderer>().material.SetColor("_Color", color);
+            abilitypower -= 0.1;
+            if (color.a >= 0)
+            {
+                color.a -= 0.03f;
+            }
+        }
+        if (isInvisible == false && color.a <= 1)
+        {
+            GetComponent<Renderer>().material.SetColor("_Color", color);
+            color.a += 0.05f;
+        }
+        if (color.a >= 0.999 || color.a <= 0.001)
+        {
+            inProgress = false;
+        }
+        if (abilitypower <= 0)
+        {
+            isInvisible = false;
+            abilitypower = 0;
+        }
+    }
+}
---
+++ Biomimicry Game/Assets/Scripts/Movement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float maxSpeed;
+    public float accelerationSpeed;
+    public float currentSpeed;
+   
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        float move = Input.GetAxis("Horizontal");
+
+        var move1 = new Vector3(Input.GetAxis("Horizontal"), 0);
+
+        transform.position += move1 * maxSpeed * Time.deltaTime;
+
+        currentSpeed = move * accelerationSpeed;
+    }
+}
