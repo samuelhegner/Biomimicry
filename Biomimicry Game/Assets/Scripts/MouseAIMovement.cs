8d6fe41c6416c5e0a35e060ce9a892849e43c0c9
Patnjop <36369847+Patnjop@users.noreply.github.com>
2018-02-13 16:43:13 +0000

    AI implemented
    Still has a few issues but currently mouse moves randomly back and forth if he gets close enough to the player he speeds up to show detection works

---
+++ Biomimicry Game/Assets/Scripts/MouseAIMovement.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseAIMovement : MonoBehaviour {
+
+    private bool idle;
+    public GameObject Mouse;
+    public bool startled;
+    float xMovement;
+    float rnd = 0;
+    float xScale;
+    float tick;
+
+    void Start () {
+        idle = true;
+        xMovement = -0.02f;
+        xScale = this.transform.localScale.x;
+    }
+	
+	void Update () {
+        StartCoroutine("Timer");
+        if (idle == true)
+        {
+            Mouse.transform.position = transform.position + new Vector3(xMovement,0,0);
+            Mouse.transform.localScale = new Vector3(xScale, this.transform.localScale.y, this.transform.localScale.y);
+        }
+        if (startled == true)
+        {
+            Mouse.transform.position = transform.position + new Vector3(xMovement*8, 0, 0);
+        }
+        if (this.tag == "Startled")
+        {
+            idle = false;
+            startled = true;
+        }
+	}
+    void Timer()
+    {
+        tick += Time.deltaTime;
+        if (tick >= 1)
+        {
+            rnd = Random.Range(0, 4);
+            print(rnd);
+            if (rnd == 1)
+            {
+                xMovement = -xMovement;
+                xScale = -xScale;
+            }
+            tick = 0;
+        }
+    }
+}
