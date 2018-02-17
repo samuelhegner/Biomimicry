9915a126cdbd875cb7c2029b2316f15605059589
samuelhegner <C16439262@mydit.ie>
2018-02-17 12:57:50 +0000

    ?

---
+++ Biomimicry Game/Assets/DayNightController.cs
@@ -0,0 +1,26 @@
+﻿// ping-pong animate background color
+using UnityEngine;
+using System.Collections;
+
+public class ExampleClass : MonoBehaviour
+{
+	public Color color1 = Color.red;
+	public Color color2 = Color.blue;
+	public float duration = 3.0F;
+
+
+	public GameObject mainCam;
+	Camera cam;
+
+	void Start()
+	{
+		cam = GetComponent<Camera>();
+		cam.clearFlags = CameraClearFlags.SolidColor;
+	}
+
+	void Update()
+	{
+		float t = Mathf.PingPong(Time.time, duration) / duration;
+		cam.backgroundColor = Color.Lerp(color1, color2, t);
+	}
+}
