a5b4efcbf9554e3a877b3f4899f8c7d6bbc3e0f6
samuelhegner <C16439262@mydit.ie>
2018-03-14 16:11:52 +0000

    Bird Predator Working

--- Biomimicry Game/Assets/Scripts/PredatorBird.cs
+++ Biomimicry Game/Assets/Scripts/PredatorBird.cs
@@ -9,13 +9,24 @@ public class PredatorBird : MonoBehaviour
     Transform PlayerTransform;
     Transform PlayerBody;
 
+
+    bool activeBird;
+    bool reachedPlayer;
+
     float EagleX;
     float EagleY;
 
     float timer;
     float rnd;
+    float startTime;
+    float distCovered;
+    float fracJourney;
+    float journeyLength;
 
-    Vector3 UpdatedPlayerPosition = new Vector3(0,20,0);
+
+    Vector3 birdStartPos;
+    Vector3 birdEndPos;
+    Vector3 UpdatedPlayerPosition;
 
     void Start()
     {
@@ -23,6 +34,9 @@ public class PredatorBird : MonoBehaviour
         PlayerBody = GameObject.Find("Character Body").GetComponent<Transform>();
         EagleX = PlayerTransform.position.x + 12;
         EagleY = PlayerTransform.position.y + 10;
+
+        activeBird = false;
+        timer = 0;
     }
 
 
@@ -31,33 +45,74 @@ public class PredatorBird : MonoBehaviour
 
         PlayerTransform = GameObject.Find("Character Eyes").GetComponent<Transform>();
 
-        if (DayTimeTracker.daytime == true)
+        if (DayTimeTracker.daytime == true && activeBird == false)
         {
             Invoke("Timer", 0);
+            Eagle.transform.position = new Vector3(PlayerTransform.position.x, 1000f, 0);
         }
-        if (DayTimeTracker.daytime == false)
+        else if (DayTimeTracker.daytime == false)
         {
-            Eagle.transform.position = new Vector3(PlayerTransform.position.x, PlayerTransform.position.y + EagleY, 0);
+            Eagle.transform.position = new Vector3(PlayerTransform.position.x, 1000f, 0);
+        }
+
+        print(reachedPlayer);
+
+        if (activeBird == true)
+        {
+            if (reachedPlayer == false)
+            {
+                float distCovered = (Time.time - startTime) * 10f;
+                float fracJourney = distCovered / journeyLength;
+                this.transform.position = Vector3.Lerp(birdStartPos, UpdatedPlayerPosition, fracJourney);
+
+                if (this.transform.position == UpdatedPlayerPosition)
+                {
+                    reachedPlayer = true;
+                    Invoke("SetTime", 0);
+                }
+            }
+            else {
+                float distCovered = (Time.time - startTime) * 10f;
+                float fracJourney = distCovered / journeyLength;
+                this.transform.position = Vector3.Lerp(UpdatedPlayerPosition, birdEndPos, fracJourney);
+
+                if (this.transform.position == birdEndPos)
+                {
+                    reachedPlayer = false;
+                    activeBird = false;
+                    Invoke("SetTime", 0);
+                }
+            }
         }
     }
 
     void Timer()
     {
         timer += Time.deltaTime;
+
         if (timer >= 1)
         {
             rnd = Random.Range(0, 4);
             if (rnd == 0)
             {
+                activeBird = true;
+                Invoke("SetTime", 0);
                 Invoke("SetPlayerPosition", 0);
-                Eagle.transform.position = new Vector3(UpdatedPlayerPosition.x + 12, UpdatedPlayerPosition.y + 10, 0);
             }
+
             timer = 0;
         }
     }
 
     void SetPlayerPosition()
     {
-        UpdatedPlayerPosition = PlayerTransform.position;
+        UpdatedPlayerPosition = new Vector3(PlayerTransform.position.x, PlayerTransform.position.y, 0);
+        birdStartPos = new Vector3(UpdatedPlayerPosition.x + 10f, UpdatedPlayerPosition.y + 10f, 0);
+        birdEndPos = new Vector3(UpdatedPlayerPosition.x - 10f, UpdatedPlayerPosition.y + 10f, 0);
+        journeyLength = Vector3.Distance(birdStartPos, UpdatedPlayerPosition);
+    }
+
+    void SetTime() {
+        startTime = Time.time;
     }
 }
