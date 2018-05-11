cdf96ec913d143c5a73566dd1caf8704efb840f2
samuelhegner <C16439262@mydit.ie>
2018-05-11 17:02:16 +0100

    Animation Added

---
+++ Biomimicry Game/Assets/DestroyAfterAnimationScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyAfterAnimationScript : MonoBehaviour {
+    Animator anim;
+    // Update is called once per frame
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+ 
+
+	void Update () {
+        if (!anim.GetCurrentAnimatorStateInfo(0).IsName("Puff"))
+        {
+            Destroy(gameObject);
+        }
+	}
+}
