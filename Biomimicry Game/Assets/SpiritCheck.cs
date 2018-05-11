546c3866d66bd1d7cf123f4ba09a64b38b7b6f61
Patnjop <36369847+Patnjop@users.noreply.github.com>
2018-05-11 22:53:34 +0100

    d

--- Biomimicry Game/Assets/SpiritCheck.cs
+++ Biomimicry Game/Assets/SpiritCheck.cs
@@ -5,15 +5,5 @@ using UnityEngine.SceneManagement;
 
 public class SpiritCheck : MonoBehaviour {
 
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.tag == "Player")
-        {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-        }
-        if (collision.tag == "Bird")
-        {
-            Destroy(this.gameObject);
-        }
-    }
+   
 }
