723a5980b7cec43a6e5de01ce79716e3885261ff
Patnjop <36369847+Patnjop@users.noreply.github.com>
2018-05-13 20:56:23 +0100

    Bugs fixed ;)

---
+++ Biomimicry Game/Assets/Scripts/NewCricketJump.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewCricketJump : MonoBehaviour {
+
+    Transform Player;
+    Transform PlayerBody;
+    public Transform QueenTransform;
+    public float queenStartY;
+    public float jumpHeight;
+    float Timer;
+
+    Rigidbody2D rb;
+    Animator anim;
+
+    public enum CricketBehaviour
+    {
+        idle,
+        falling,
+        jumping,
+        encouraged,
+    }
+
+    public CricketBehaviour currentBehaviour;
+
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        Player = GameObject.Find("Player").GetComponent<Transform>();
+        PlayerBody = GameObject.Find("Character Body").GetComponent<Transform>();
+        currentBehaviour = CricketBehaviour.idle;
+        queenStartY = QueenTransform.position.y;
+        anim = GetComponent<Animator>();
+
+        int ran = Random.Range(1, 3);
+
+        if (ran == 1)
+        {
+            Vector3 Scale = new Vector3(this.transform.localScale.x, this.transform.localScale.y, this.transform.localScale.z);
+            Scale.x *= -1;
+            transform.localScale = Scale;
+        }
+    }
+	
+	
+	void Update () {
+        float vSpeed = rb.velocity.y;
+
+        anim.SetFloat("vSpeed", vSpeed);
+
+        print(Player.position.x);
+        if (DayTimeTracker.daytime == false)
+        {
+            if (QueenTransform.position.y > queenStartY && currentBehaviour == CricketBehaviour.idle)
+            {
+                currentBehaviour = CricketBehaviour.encouraged;
+            }
+            if (currentBehaviour == CricketBehaviour.encouraged)
+            {
+                Timer += Time.deltaTime;
+                if (Timer > 0.5)
+                {
+                    Timer = 0;
+                    if (PlayerBody.tag == "Unstealthed" && Player.position.x > transform.position.x + 8 || PlayerBody.tag == "Unstealthed" && Player.position.x < transform.position.x - 8)
+                    {
+                        currentBehaviour = CricketBehaviour.jumping;
+                    }
+                    else if (PlayerBody.tag == "Stealthed")
+                    {
+                        currentBehaviour = CricketBehaviour.jumping;
+                    }
+                    else if (PlayerBody.tag == "Unstealthed" && Player.position.x < transform.position.x + 8 && Player.position.x > transform.position.x - 8)
+                    {
+                        currentBehaviour = CricketBehaviour.idle;
+                    }          
+                }
+            }
+            if (currentBehaviour == CricketBehaviour.idle)
+            {
+                this.tag = "Untagged";
+                anim.SetBool("Idle", true);
+            }
+            if (currentBehaviour == CricketBehaviour.jumping)
+            {
+                anim.SetBool("Idle", false);
+                rb.AddForce(Vector2.up * jumpHeight, ForceMode2D.Impulse);
+                currentBehaviour = CricketBehaviour.falling;
+            }
+            if (currentBehaviour == CricketBehaviour.falling)
+            {
+                if (rb.velocity.y > 0.1 || rb.velocity.y < -0.1)
+                {
+                    this.tag = "NPC";
+                }
+                else if (rb.velocity.y <= 0.1 || rb.velocity.y >=-0.1)
+                {
+                    this.tag = "Untagged";
+                    currentBehaviour = CricketBehaviour.idle;
+                }              
+            }
+        }
+        if (DayTimeTracker.daytime == false)
+        {
+
+        }
+	}
+}
---
+++ Biomimicry Game/Assets/Scripts/NewQueenJump.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewQueenJump : MonoBehaviour {
+
+    Animator anim;
+    Rigidbody2D rigid;
+
+    float rnd;
+    float Timer = 0;
+    public float jumpHeight;
+    int maxRange = 5;
+
+    public enum QueenBehaviour
+    {
+        idle,
+        jumping,
+        falling,
+    }
+
+    public QueenBehaviour currentQueenState;
+
+    void Start () {
+        rigid = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        currentQueenState = QueenBehaviour.idle;
+
+        int ran = Random.Range(1, 3);
+
+        if (ran == 1)
+        {
+            Vector3 Scale = new Vector3(this.transform.localScale.x, this.transform.localScale.y, this.transform.localScale.z);
+            Scale.x *= -1;
+            transform.localScale = Scale;
+        }
+    }
+	
+	void Update () {
+        print(rnd);
+        float vSpeed = rigid.velocity.y;
+
+        anim.SetFloat("vSpeed", vSpeed);
+
+        if (currentQueenState == QueenBehaviour.idle)
+        {
+            Timer += Time.deltaTime;
+        }
+        if (Timer >= 1 && currentQueenState == QueenBehaviour.idle)
+        {
+            rnd = Random.Range(1, maxRange);
+            Timer = 0;
+        }
+        if (rnd == 3)
+        {
+            rnd = 0;
+            if (DayTimeTracker.daytime == false)
+            {
+                currentQueenState = QueenBehaviour.jumping;
+            }
+        }
+        
+
+        if (currentQueenState == QueenBehaviour.idle)
+        {
+            anim.SetBool("Idle", true);
+        }
+        if (currentQueenState == QueenBehaviour.jumping)
+        {
+            anim.SetBool("Idle", false);
+            rigid.AddForce(transform.up * jumpHeight, ForceMode2D.Impulse);
+            currentQueenState = QueenBehaviour.falling;
+        }
+        if (currentQueenState == QueenBehaviour.falling)
+        {
+            if (rigid.velocity.y < 0.1)
+            {
+                Timer = 0;
+                currentQueenState = QueenBehaviour.idle;
+            }
+        }
+    }
+}
