1f41a99b463adaaedf43eae607c1edff935021b9
Patnjop <36369847+Patnjop@users.noreply.github.com>
2018-05-13 15:13:50 +0100

    Glide Fix

--- Biomimicry Game/Assets/Scripts/Jumping.cs
+++ Biomimicry Game/Assets/Scripts/Jumping.cs
@@ -59,7 +59,7 @@ public class Jumping : MonoBehaviour
             }
         }
 
-        if (jumpCount == 2 && !canJump)
+        if (jumpCount == 2 && !canJump && objectiveCounter >= 3)
         {
             if (Input.GetKey(KeyCode.LeftShift))
             {
