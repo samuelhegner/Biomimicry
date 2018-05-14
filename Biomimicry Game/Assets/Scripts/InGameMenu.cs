fc6c22c704c91f0eac92afb2f860d3c2a9808ebe
Patnjop <36369847+Patnjop@users.noreply.github.com>
2018-05-14 16:42:39 +0100

    Menu

---
+++ Biomimicry Game/Assets/Scripts/InGameMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InGameMenu : MonoBehaviour {
+
+    bool Pause;
+
+    public GameObject pauseMenu;
+	
+	void Update () {
+
+        if (Pause == true)
+        {
+            pauseMenu.SetActive(true);
+            Time.timeScale = 0f;
+        }
+
+        if (Pause == false)
+        {
+            pauseMenu.SetActive(false);
+            Time.timeScale = 1f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Pause = !Pause;
+        }
+	}
+}
